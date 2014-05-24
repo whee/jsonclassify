bd43ad054d73a80088e3b7e92b320fe17f16b834
Brian Hetro <whee@smaertness.net>
2014-05-24 19:04:45 -0400

    Clean up classifier creation

--- jsonclassify.go
+++ jsonclassify.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -22,21 +21,24 @@ type Category struct {
 	Attributes map[string][]int
 }
 
+func NewClassifier(file string) (*Classifier, error) {
+	configData, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	var c Classifier
+	err = json.Unmarshal(configData, &c)
+	return &c, err
+}
+
 func main() {
 	if len(os.Args) != 2 {
 		log.Fatal("Usage: jsonclassify [configuration file]\n")
 	}
-
 	configFile := os.Args[1]
-	configData, err := ioutil.ReadFile(configFile)
+	c, err := NewClassifier(configFile)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var c Classifier
-	if err := json.Unmarshal(configData, &c); err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(c)
 }
